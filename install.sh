78794e3b56278f65bd9411054d89d3e78914417a
hari-robotics <211106370@qq.com>
2025-04-25 19:30:42 +0200

    fix an error that will cause installing git failed

--- install.sh
+++ install.sh
@@ -1,4 +1,62 @@
 #!/bin/bash
+try_install() {
+    local package="$1"
+    local display_name="${2:-$package}"
+
+    echo -ne "Trying to install $display_name: "
+    if command -v "$package" >/dev/null 2>&1; then
+        echo "$display_name is already installed."
+        return 0
+    fi
+
+    sudo apt install -y "$package" > /dev/null 2>&1 &
+    local pid=$!
+
+    local spin='/-\|'
+    while kill -0 "$pid" 2>/dev/null; do
+        for (( i=0; i<${#spin}; i++ )); do
+            echo -ne "\rInstalling $display_name ${spin:$i:1}"
+            sleep 0.1
+        done
+    done
+
+    wait "$pid"  # 等待安装进程完成
+
+    if [ $? -eq 0 ]; then
+        echo -e " Installed"
+    else
+        echo -e " Failed"
+        error "Installation failed. Please install $display_name manually."
+        exit 1
+    fi
+}
+
+try_add_apt_repository() {
+    local repository="$1"
+    local display_name="${2:-$repository}"
+
+    sudo add-apt-repository -y "$repository" > /dev/null 2>&1 &
+    local pid=$!
+
+    local spin='/-\|'
+    while kill -0 "$pid" 2>/dev/null; do
+        for (( i=0; i<${#spin}; i++ )); do
+            echo -ne "\rAdding $display_name to apt repository ${spin:$i:1}"
+            sleep 0.1
+        done
+    done
+
+    wait "$pid"  # 等待安装进程完成
+
+    if [ $? -eq 0 ]; then
+        echo -e " Added"
+    else
+        echo -e " Failed"
+        error "Adding repository failed. Please try adding $display_name manually."
+        exit 1
+    fi
+}
+
 source $HOME/.tmp_install/scripts/functions.sh
 
 $SCRIPTS_DIR=$HOME/.tmp_install/install
